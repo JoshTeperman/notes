3c07855320b39e602593f78196c7d09c8701ff7f
Josh Teperman <joshteperman@gmail.com>
2019-09-24 08:37:05 +1000

    jwt_devise_on_rails.rb

---
+++ jwt_devise_on_rails.rb
@@ -0,0 +1,2 @@
+https://medium.com/@nandhae/2019-how-i-set-up-authentication-with-jwt-in-just-a-few-lines-of-code-with-rails-5-api-devise-9db7d3cee2c0
+https://stackoverflow.com/questions/37770467/why-do-i-have-to-git-push-set-upstream-origin-branch
